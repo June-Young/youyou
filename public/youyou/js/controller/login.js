1d0aed6dde9ec5a5e35c8cd80c304b164d2997bd
variable-kang <variable_kang@naver.com>
2017-10-22 21:34:22 +0900

    login 후 페이지 이동이 안되고 있던거 고침 apply 스코프 안에 넣어야 변경 바로 적용됨

--- public/youyou/js/controller/login.js
+++ public/youyou/js/controller/login.js
@@ -1,26 +1,33 @@
 'use strict';
 // var app = angular.module('MobileAngularUiExamples');
-angular.module('MobileAngularUiExamples').controller('login', function ($scope, $location) {
-  $scope.signInGoogle = function ($location) {
+var app = angular.module('MobileAngularUiExamples');
+app.controller('login', function ($scope, $location) {
+  $scope.signInGoogle = function () {
     console.log('signin google');
 
     var google = new firebase.auth.GoogleAuthProvider();
     firebase.auth().signInWithPopup(google).then(function () {
       console.log('login : ' + firebase.auth().currentUser);
       // window.location.replace("/youyou/#/nickname");
-      $location.path("nickname");
+      pageChange();
     });
 
   };
-  $scope.signInFacebook = function ($location) {
+  $scope.signInFacebook = function () {
     console.log('signin facebook');
     var facebook = new firebase.auth.FacebookAuthProvider();
     firebase.auth().signInWithPopup(facebook).then(function () {
       console.log('login : ' + firebase.auth().currentUser);
       // window.location.replace("/youyou/#/nickname");
-      $location.path("nickname");
+      pageChange()
     });
   };
+
+  var pageChange = function () {
+    $scope.$apply(function () {
+      $location.path("nickname");
+    });
+  }
 });
 
 angular.module('MobileAngularUiExamples').controller('nickname', function ($scope, $location) {
