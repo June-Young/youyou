51d7db63f0f7665cc8ea5f012694f7adc545c2e0
variable-kang <variable_kang@naver.com>
2017-10-28 18:41:30 +0900

    push 펑션생성 및 완료함. messages 데이터베이스 스키마변경됨

--- public/youyou/js/controller/home.js
+++ public/youyou/js/controller/home.js
@@ -24,16 +24,49 @@ app.controller('HomeController', function ($scope, $location) {
     $location.path("profile");
   };
 
+  const messaging = firebase.messaging();
+  let permission = function () {
+    messaging.requestPermission()
+      .then(function () {
+        console.log("permission grated");
+        getToken();
+      }).catch(function (err) {
+      console.log('Unable to get permission to notify.' + err);
+    });
+  };
+
+  let getToken = function (id) {
+    messaging.getToken()
+      .then(function (currentToken) {
+        //DB확인하고 없으면 넣는다.
+        var device = navigator.userAgent || 'Unknown';
+        var data = [];
+        data[currentToken] = device;
+        firebase.database().ref('users/' + id + '/notifications').set(data);
+        console.log(navigator.userAgent);
+        if (currentToken) {
+          console.log('current ' + currentToken);
+        } else {
+          permission();
+        }
+      });
+  };
+
+  messaging.onMessage(function(payload){
+    console.log('onMessage : '+payload);
+  });
 
   var user = firebase.auth().currentUser;
   if (user) {
-    firebase.database().ref('users/' + user.uid).once('value').then(function (userDetailSnapshot) {
+    const uid = user.uid;
+    firebase.database().ref('users/' + uid).once('value').then(function (userDetailSnapshot) {
       var displayName = userDetailSnapshot.val() && userDetailSnapshot.val().displayName || 'Unknown';
       $scope.$apply(function () {
         $scope.nickname = displayName;
       });
     });
-  }else{
+    getToken(uid);
+  } else {
     console.error("인가되지 않은 유저입니다. 로그인 해주세요.");
     $location.path("login");
   }
