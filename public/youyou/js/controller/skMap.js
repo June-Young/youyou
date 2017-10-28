acbe1dbd77a270eaeaf6dd644c65b95286d394f1
junyeong.han <junyeong.han@linecorp.com>
2017-10-28 22:26:37 +0900

    interface between map and chat 추가

--- public/youyou/js/controller/skMap.js
+++ public/youyou/js/controller/skMap.js
@@ -1,12 +1,45 @@
-angular.module('MobileAngularUiExamples').controller('SkMapController', ['$scope', '$http', function ($scope) {
-  $scope.posCount = 0;
-  function init() {
+angular.module('MobileAngularUiExamples').controller('SkMapController', function ($scope, $location) {
+
+  function initialize() {
+    //route flag
+    $scope.posCount = 0;
+
     $scope.map = new Tmap.Map({div: "map_div", width: '100%', height: '100%'});
     $scope.markerLayer = new Tmap.Layer.Markers();
     $scope.map.addLayer($scope.markerLayer);
     $scope.map.setLanguage("EN", false); // 영문
     $scope.map.setCenter(new Tmap.LonLat(14135016.577353, 4518074.1072027), 16);
-    //default : City hall station
+    getCurrentLocation();
+    setMapTouchEvent();
+    getParameters();
+  }
+
+  function initializeWithShare(startX, startY) {
+    // ?type=share&sx=14152221.255693141&sy=4496551.406476195
+    makeMarker(new Tmap.LonLat(startX, startY));
+  }
+
+  function initializeWithRoute(startX, startY, endX, endY) {
+    // ?type=route&sx=14152221.255693141&sy=4496551.406476195&ex=14135016.577353&ey=4518074.1072027
+    getRouteWithLocations(new Tmap.LonLat(startX, startY), new Tmap.LonLat(endX, endY));
+  }
+
+  function getParameters() {
+    var urlParams = $location.search();
+
+    console.log("getParameters => type :  " + urlParams.type +
+      " , sx : " + urlParams.sx + " , " +
+      " , sy : " + urlParams.sy + " , " +
+      " , ex : " + urlParams.ex + " , " +
+      " , ey : " + urlParams.ey + " , ");
+
+    if (!urlParams.type) {
+      console.log("null");
+    } else if (urlParams.type == "share") {
+      initializeWithShare(urlParams.sx, urlParams.sy);
+    } else if (urlParams.type == "route") {
+      initializeWithRoute(urlParams.sx, urlParams.sy, urlParams.ex, urlParams.ey);
+    }
   }
 
   function removePreviousRouteLay() {
@@ -47,8 +80,9 @@ angular.module('MobileAngularUiExamples').controller('SkMapController', ['$scope
   }
 
   function processLocation(position) {
-    console.log("latitude : " + position.coords.latitude + " , longtitude : " + position.coords.longitude);
     $scope.myLonLat = transformLonLat(position.coords.longitude, position.coords.latitude);
+    // $scope.map.setCenter($scope.myLonLat, 16);
+    console.log("myLocation = " + $scope.myLonLat.toString());
   }
 
   function makeMarker(lonlat) {
@@ -62,11 +96,11 @@ angular.module('MobileAngularUiExamples').controller('SkMapController', ['$scope
       $scope.posCount = 1;
     } else if ($scope.posCount == 1) {
       $scope.endLonLat = lonlat;
-      getRouteFromMarker($scope.startLonLat, $scope.endLonLat);
+      getRouteWithLocations($scope.startLonLat, $scope.endLonLat);
       $scope.posCount = 0;
     }
 
-    console.log("makeMarker : " + lonlat + " , " + $scope.posCount);
+    console.log("makeMarker : " + lonlat.toString + " , " + $scope.posCount);
 
     var size = new Tmap.Size(24, 38);
     var offset = new Tmap.Pixel(-(size.w / 2), -(size.h / 2));
@@ -98,31 +132,8 @@ angular.module('MobileAngularUiExamples').controller('SkMapController', ['$scope
     $scope.tourList = getTourInfoFromFireBase();
 
     for (var index = 0; index < $scope.tourList.length; index++) {
-      setMarker($scope.tourList[index].lonlat);
+      // setMarker($scope.tourList[index].lonlat);
     }
-
-  }
-
-  function setMarker(lonlat) {
-    var size = new Tmap.Size(24, 38);
-    var offset = new Tmap.Pixel(-(size.w / 2), -(size.h / 2));
-    /*
-     marker에 표시할 icon객체는 'https://developers.skplanetx.com/upload/tmap/marker/pin_b_m_a.png'의 경로에 있는 png파일을 가리키는데
-     24픽셀 세로 38픽셀의 크기를 가지고 원래 위치에서 x축으로 -24/2만큼, y축으로 -38만큼 움직여서 표시하라 는 코드 입니다.
-     */
-    var icon = new Tmap.Icon('https://developers.skplanetx.com/upload/tmap/marker/pin_b_m_a.png', size, offset);
-    // image src 는 이렇게 ~
-    // var icon = new Tmap.IconHtml("<img src= 'https://developers.skplanetx.com/upload/tmap/marker/pin_b_m_a.png'></img>", size, offset);
-
-    var marker = new Tmap.Marker(lonlat, icon);
-    $scope.currentMarker = marker;
-    $scope.markerLayer.addMarker(marker);
-    // 위처럼 소스를 입력하면 마커가 생김
-    // 아래 소스를 넣으면 센터 위치변경
-    marker.events.register("click", marker, onMouseMarkerEvent);
-    marker.events.register("touchend", marker, onMouseMarkerEvent);
-    marker.events.register("mouseover", marker, onMouseMarkerEvent);
-    marker.events.register("mouseout", marker, onMouseMarkerEvent);
   }
 
   function transformLonLat(lontitude, latitude) {
@@ -135,40 +146,38 @@ angular.module('MobileAngularUiExamples').controller('SkMapController', ['$scope
     return resultLonLat;
   }
 
-  function addMapEvent() {
-    // $scope.map.events.register("click", $scope.map, onClickMap);
+  function setMapTouchEvent() {
+    $scope.touchMoveCount = 0;
     $scope.map.events.register("touchstart", $scope.map, onTouchStart);
     $scope.map.events.register("touchend", $scope.map, onTouchEnd);
     $scope.map.events.register("touchmove", $scope.map, onTouchMove);
     $scope.map.events.register("touchcancel", $scope.map, onTouchCancel);
-  }
 
-  function onTouchCancel(evt) {
-    console.log("onTouchCancel event location " + evt);
-  }
-
-  function onTouchStart(evt) {
-    console.log("onTouchStart event location " + evt);
-  }
-
-  $scope.touchMoveCount = 0;
-  function onTouchMove(evt) {
-    console.log("onTouchMove event location : " + evt);
-    $scope.touchMoveCount++;
-  }
-
-  function onTouchEnd(evt) {
-    console.log("onTouchEnd event location : " + evt.xy.x + ", " + evt.xy.y);
-    if ($scope.touchMoveCount > 3) {
-      console.log("it might just move action");
-    } else {
-      makeMarker(getLonLatFromMapLocation(evt.xy.x, evt.xy.y));
+    function onTouchCancel(evt) {
+      console.log("onTouchCancel event location " + evt);
     }
-    $scope.touchMoveCount = 0;
+
+    function onTouchStart(evt) {
+      console.log("onTouchStart event location " + evt);
+    }
+
+    function onTouchMove(evt) {
+      console.log("onTouchMove event location : " + evt);
+      $scope.touchMoveCount++;
+    }
+
+    function onTouchEnd(evt) {
+      console.log("onTouchEnd event location : " + evt.xy.x + ", " + evt.xy.y);
+      if ($scope.touchMoveCount > 3) {
+        console.log("it might just move action");
+      } else {
+        makeMarker(getLonLatFromMapLocation(evt.xy.x, evt.xy.y));
+      }
+      $scope.touchMoveCount = 0;
+    }
   }
 
   function getLonLatFromMapLocation(clientX, clientY) {
-    // it will return Tmap.LonLat
     return $scope.map.getLonLatFromPixel(
       new Tmap.Pixel(clientX, clientY));
   }
@@ -186,25 +195,32 @@ angular.module('MobileAngularUiExamples').controller('SkMapController', ['$scope
     }
   }
 
-  function getRouteFromMarker(startLonLat, endLonLat) {
+  function getRouteWithLocations(startLonLat, endLonLat) {
     routeLayerInit();
 
     var tData = new Tmap.TData();
-
     tData.getRoutePlan(startLonLat, endLonLat);
     tData.events.register("onComplete", tData, onComplete);
     tData.events.register("onProgress", tData, onProgress);
     tData.events.register("onError", tData, onError);
-  }
 
-  function onComplete() {
-    routeLayerInit();
-    var kmlForm = new Tmap.Format.KML().read(this.responseXML);
-    for (var i = 0; i < kmlForm.length; i++) {
-      console.log("kmlForm[ " + i + " ]  : " + kmlForm[i].data.distance + " , description = " + kmlForm[i].data.name + " , " + kmlForm[i].data.description);
-      $scope.kmlLayer.addFeatures([kmlForm[i]]);
+    function onProgress() {
+      console.log("onProgress");
     }
-    setTimeout(stop, 500)
+
+    function onError() {
+      console.log("onError");
+    }
+
+    function onComplete() {
+      routeLayerInit();
+      var kmlForm = new Tmap.Format.KML().read(this.responseXML);
+      for (var i = 0; i < kmlForm.length; i++) {
+        console.log("kmlForm[ " + i + " ]  : " + kmlForm[i].data.distance + " , description = " + kmlForm[i].data.name + " , " + kmlForm[i].data.description);
+        $scope.kmlLayer.addFeatures([kmlForm[i]]);
+      }
+      setTimeout(stop, 500)
+    }
   }
 
   function popupTest(index, lonlat) {
@@ -217,28 +233,6 @@ angular.module('MobileAngularUiExamples').controller('SkMapController', ['$scope
     popup.show();
   }
 
-  function onProgress() {
-    console.log("onProgress");
-  }
-
-  function onError() {
-    console.log("onError");
-  }
-
-  function addEventExample() {
-    console.log("addEventExample()");
-    var popup = new Tmap.Popup("p1",
-      new Tmap.LonLat(14152157.580944417, 4499026.955219546),
-      new Tmap.Size(100, 150),
-      "<div id='popupID'>1<button onclick='popButtonClick()'>Button</button></div>"
-    );
-    $scope.map.addPopup(popup);
-  }
-
-  init();
-  getCurrentLocation($scope.map);
-  addMapEvent();
-
   $scope.proxyMethod = function () {
     $scope.markerFlag = false;
     console.log("proxy Method Call . ");
@@ -248,19 +242,12 @@ angular.module('MobileAngularUiExamples').controller('SkMapController', ['$scope
     $scope.markerFlag = false;
     console.log("proxy Method Call : " + eventName);
   }
-}]);
+
+  initialize();
+
+});
 
 function popupProxyMethod1() {
   console.log("popupButtonClick1 : " + this);
   angular.element(document.getElementById('popupID')).scope().proxyMethod("popupButtonClick1");
 }
-function popupProxyMethod2() {
-  console.log("popupButtonClick2 : " + this);
-  angular.element(document.getElementById('popupID')).scope().proxyMethod("popupButtonClick2");
-}
-function popupProxyMethod3() {
-  console.log("popupButtonClick3: " + this);
-  angular.element(document.getElementById('popupID')).scope().proxyMethod("popupButtonClick3");
-}
-
-
